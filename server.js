a5f40e38c99bbba81a37c0468a8b6b37684272db
Paulo Victor <vitorcanalle@gmail.com>
2025-03-27 20:41:48 -0300

    Criação dos parametros PUT e DELETE

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import express from 'express'
-import { PrismaClient }  from '@prisma/client'
+import {
+  PrismaClient
+} from '@prisma/client'
 
 /*
   get => Listar
@@ -13,7 +15,6 @@ const prisma = new PrismaClient()
 const app = express()
 app.use(express.json())
 
-const users = []
 
 app.post('/usuarios', async (req, res) => {
 
@@ -29,13 +30,50 @@ app.post('/usuarios', async (req, res) => {
 })
 
 
-app.get('/usuarios', (req, res) => {
+app.get('/usuarios', async (req, res) => {
+
+  const users = await prisma.user.findMany()
+
   res.status(200).json(users)
+})
+
+
+
+app.put('/usuarios/:id', async (req, res) => {
+
+
+  await prisma.user.update({
+
+    where: {
+      id: req.params.id
+    },
+    data: {
+      email: req.body.email,
+      name: req.body.name,
+      age: req.body.age
+    }
+  })
+
+  res.status(201).json(req.body)
 })
 
 /*
   1) Tipo de rota / Método HTTP
   2) Endereço
 */
+
+app.delete('/usuarios/:id', async (req, res) => {
+
+  await prisma.user.delete({
+
+    where: {
+      id: req.params.id
+    }
+  })
+
+  res.status(200).json({
+    message: " Usuario deletado com Sucesso! "
+  })
+})
 
 app.listen(3000)
